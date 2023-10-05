3a150ee25ea0abfbb617a9ba63ef867d8a71fd4d
Rus <bedlinskiyruslan@gmail.com>
2023-10-05 13:28:41 +0300

    HistoryList configured

--- src/components/appointmentItem.tsx/AppointmentItem.tsx
+++ src/components/appointmentItem.tsx/AppointmentItem.tsx
@@ -6,7 +6,7 @@ import { Optional } from "utility-types";
 import "./appointmentItem.scss";
 
 type AppointmentProps = Optional<IAppointment, "canceled"> & {
-	openModal: (state: number) => void;
+	openModal?: (state: number) => void;
 	getActiveAppointments?: () => void;
 };
 
@@ -57,7 +57,7 @@ const AppointmentItem = memo(
 					<span className="appointment__service">Service: {service}</span>
 					<span className="appointment__phone">Phone: {phone}</span>
 				</div>
-				{!canceled ? (
+				{!canceled && openModal ? (
 					<>
 						<div className="appointment__time">
 							<span>Time left:</span>
--- src/components/historyList/HistoryList.tsx
+++ src/components/historyList/HistoryList.tsx
@@ -1,10 +1,54 @@
+import { useContext, useEffect } from "react";
+
 import AppointmentItem from "../appointmentItem.tsx/AppointmentItem";
+import Spinner from "../spinner/Spinner";
+import Error from "../error/Error";
+
+import { AppointmentContext } from "../../context/appointments/AppointmentsContext";
 
 function HistoryList() {
-	
+	const { allAppointments, getAppointments, appointmentLoadingStatus } =
+		useContext(AppointmentContext);
+
+	useEffect(() => {
+		getAppointments();
+	}, []);
+
+	if (appointmentLoadingStatus === "loading") {
+		return <Spinner />;
+	}
+	if (appointmentLoadingStatus === "error") {
+		return (
+			<>
+				<Error />
+				<button className="history__reload" onClick={getAppointments}>
+					Try to reload
+				</button>
+			</>
+		);
+	}
+
 	return (
 		<>
-			<div></div>
+			{/* {allAppointments.map((item) => {
+				return (
+					<AppointmentItem
+						{...item}
+						key={item.id}
+						openModal={() => {}}
+						page={"history"}
+					/>
+				);
+			})} */}
+			{allAppointments
+				.sort(
+					(prev, next) =>
+						new Date(prev.date).getTime() - new Date(next.date).getTime()
+				)
+				.map((item) => {
+					return <AppointmentItem {...item} key={item.id} />;
+				})}
+			{/* <div></div> */}
 			{/* <AppointmentItem />
 			<AppointmentItem />
 			<AppointmentItem />
